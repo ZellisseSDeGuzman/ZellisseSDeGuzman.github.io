ec2ea88490002715431e06f1c4345db819a35db7
ZellisseSDeGuzman <deguzmanszellisse@gmail.com>
2016-03-15 12:24:30 -0400

    text js

--- otherProjects/ajaxExamples/js/clients.js
+++ otherProjects/ajaxExamples/js/clients.js
@@ -1,31 +1,62 @@
-$(document).ready(function)({
+//$(document).ready(function)({
 
-  $("#getClients").on("click", function() {
+  //$("#getClients").on("click", function() {
 
-    var url = "http://ZellisseSDeGuzman.github.io/otherProjects/ajaxExamples/jsonDatabase/clients.json"
-    $.getJSON(url, function(data) {
-      var html ="<table class='table table-hover table-striped'>" +
-      "<tr><th>Name</tr><th>Email</th><th>Company</th></tr>";
+    //var url = "http://ZellisseSDeGuzman.github.io/otherProjects/ajaxExamples/jsonDatabase/clients.json"
+    //$.getJSON(url, function(data) {
+      //var html ="<table class='table table-hover table-striped'>" +
+      //"<tr><th>Name</tr><th>Email</th><th>Company</th></tr>";
 
 
-        //$.getJSON("http://ZellisseSDeGuzman.github.io/otherProjects/ajaxExamples/jsonDatabase/clients.json")
+        //$.getJSON("http://ZellisseSDeGuzman.github.io/otherProjects/ajaxExamples/jsonDatabase/clients.json")-
 
-        $.each(data, function(index, item) {
-          //$("#data").append(item.name);
+        //$.each(data, function(index, item) {
+          //$("#data").append(item.name);-
 
-            html += "<tr>" +
-              "<td>" + item.name + "</td>" +
-              "<td>" + item.email + "</td>" +
-              "<td>" + item.company + "</td>" +
-              "</tr>";
+            //html += "<tr>" +
+              //"<td>" + item.name + "</td>" +
+              //"<td>" + item.email + "</td>" +
+              //"<td>" + item.company + "</td>" +
+              //"</tr>";
 
+            //})
+        //alert(data);-
+        //console.dir(data);-
+
+      //}) //getJSON
+
+
+  //}) //click
+
+//})//document
+
+
+$(document).ready(function() {
+
+    $("#getClients").on("click", function() {
+
+        var url = "http://ZellisseSDeGuzman.github.io/otherProjects/ajaxExamples/jsonDatabase/clients.json";
+
+        $.getJSON(url, function(data) {
+            var html = "<table class='table table-hover table-striped'>" +
+              "<tr><th>Name</th><th>Email</th><th>Company</th></tr>";
+
+            $.each(data, function(index, item) {
+
+              html += "<tr>" +
+                "<td>" + item.name + "</td>" +
+                "<td>" + item.email + "</td>" +
+                "<td>" + item.company + "</td>" +
+                "</tr>";
             })
-        //alert(data);
-        //console.dir(data);
 
-      }) //getJSON
+            html += "</table>";
+            $("#data").append(html);
+            //alert(data);
+            //console.dir(data);
 
+          }) //getJSON
 
-  }) //click
+      }) //click
 
-})//document
+  }) //ready
