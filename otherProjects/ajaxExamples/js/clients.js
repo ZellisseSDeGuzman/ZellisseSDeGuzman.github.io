9e9f62948d3c6b4801c718c50589b5e33f6c4ce8
ZellisseSDeGuzman <deguzmanszellisse@gmail.com>
2016-03-15 11:32:13 -0400

    js

--- otherProjects/ajaxExamples/js/clients.js
+++ otherProjects/ajaxExamples/js/clients.js
@@ -1,5 +1,14 @@
 $("#getClients").on("click", function() {
 
-  alert("working");
+  var url = "http://ZellisseSDeGuzman.github.io/otherProjects/ajaxExamples/jsonDatabase/clients.json"
+  $.getJSON(url, function (data){
 
-})
+  //$.getJSON("http://ZellisseSDeGuzman.github.io/otherProjects/ajaxExamples/jsonDatabase/clients.json")
+
+  alert(data);
+  console.dir(data);
+
+})//getJSON
+
+
+})//click
