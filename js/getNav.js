61cde271de2bfc48eba100af735e487a4161e5c0
ZellisseSDeGuzman <deguzmanszellisse@gmail.com>
2016-03-15 09:38:50 -0400

    update js

--- js/getNav.js
+++ js/getNav.js
@@ -1,9 +1,28 @@
-$.get("http://zellissesdeguzman.github.io/partials/nav.html", function(data){
+//$.get("http://zellissesdeguzman.github.io/partials/nav.html", function(data){
 
-$(document).ready(function() {
+//$(document).ready(function() {
 
-  $(".container").prepend(data);
-  
-})
+  //$(".container").prepend(data);
 
-})
+//})
+
+//})
+
+
+
+$.get("http://zellissesdeguzman.github.io/partials/nav.html", function(data) {
+
+  $(document).ready(function() {
+
+    //check if this document has .container
+    var container = $(".container");
+
+    //if so add nav.html contents to the top
+    if (container) {
+      container.prepend(data)
+      container.fadeIn();
+    }
+
+  });
+
+});
