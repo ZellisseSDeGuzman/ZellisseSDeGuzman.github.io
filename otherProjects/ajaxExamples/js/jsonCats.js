3bce08fd5062dfed4339dfba2f57c126f2071ed0
ZellisseSDeGuzman <deguzmanszellisse@gmail.com>
2016-03-22 12:14:36 -0400

    week 5 good

--- otherProjects/ajaxExamples/js/jsonCats.js
+++ otherProjects/ajaxExamples/js/jsonCats.js
@@ -1,19 +1,35 @@
 $(document).ready(function() {
 
+    $.getJSON("jsonDatabase/cat.json", function(data) {
 
-  $.getJSON("jsonDatabase/jsonCats.json", function(data) {
-    alert("!")
-    console.dir(data);
-    var html = "";
+      console.dir(data);
+      var html = "";
 
     $.each(data, function(index, item) {
         html += '<div class="col-md-4 cat">' +
           '<div class="catName">' + item.name + '</div>' +
           '<div class="catType">' + item.type + '</div>' +
           '<div class="catGender">' + item.gender + '</div>' +
-          '<img src="' + item.image + '"/>';
+          '<img class="catImage" src="' + item.image + '"/>'; +
+          '<div class="commentsContainer">';
+          $.each(item.comments, function(ind, i) {
+            html += '<div class="rentersName">'+ i.username + '</div>' +
+              '<div class="rentersComment">'+ i.comment + '</div>' +
+              '<div class="rentersStars">';
 
-        html += '</div>';
+                for (var j = 1; j <= 5; j++) {
+                  if(j <= i.stars){
+                    html+='<img src="images/fullstar.png"/>';
+                  }
+                  else {
+                    html+='<img src="images/emptystar.png"/>';
+                  }
+                }
+              html=+'</div>'; //end stars
+          }) // each comment
+
+        html += '</div>' + //commentsContainer
+                '</div>'; //col-md-4
       }) //end cat
 
     $("#catData").append(html);
@@ -32,8 +48,8 @@ $(document).ready(function() {
 <div class="commentsContainer">
   //one per comment
     <div class="rentersName"></div>
-    <div class="rentersLocation"></div>
-    <div class="rentersStarts">
+    <div class="rentersComment"></div>
+    <div class="rentersStars">
       //5starts, some full - some empty
     </div> //end of starts
 </div> //end of comments container
