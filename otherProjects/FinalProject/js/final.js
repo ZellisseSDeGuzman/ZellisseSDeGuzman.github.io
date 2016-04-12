7216cd87a5fe27e72bce7e501759cb95e6d2a775
ZellisseSDeGuzman <deguzmanszellisse@gmail.com>
2016-04-12 17:14:33 -0400

    renters deleted

--- otherProjects/FinalProject/js/final.js
+++ otherProjects/FinalProject/js/final.js
@@ -30,15 +30,10 @@ $(document).ready(function() {
 
                         $.each(data, function(index, item) {
                                 html += '<div class="col-xs-12 col-md-4 jsonCat">' +
+                                    '<img class="dinoImage" src="' + item.image + '"/>' +
                                     '<div class="dinoName">' + item.name + '</div>' +
                                     '<div class="dinoCharacteristics"><small>Characteristics </small>' + item.genre + '</div>' +
-                                    '<img class="dinoImage" src="' + item.image + '"/>' +
-                                    //deleted commentsContainer
-                                    //'<div class="panel panel-default">' + //added
-                                    //'<div class="panel-heading">Renter Comments</div>'; //added
-
-
-                                //html += '</div>' + //panel
+                                    
                                     '</div>'; //col-md-4
                             }); //each cat
 
