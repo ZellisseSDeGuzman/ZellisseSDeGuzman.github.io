f9d46f5c2803e24ad85a2717d36cf7dc258c722f
ZellisseSDeGuzman <deguzmanszellisse@gmail.com>
2016-04-12 15:57:16 -0400

    undo

--- otherProjects/FinalProject/js/final.js
+++ otherProjects/FinalProject/js/final.js
@@ -33,6 +33,26 @@ $(document).ready(function() {
                                     '<div class="dinoName">' + item.name + '</div>' +
                                     '<div class="dinoGenre"><small>Genre </small>' + item.genre + '</div>' +
                                     '<img class="dinoImage" src="' + item.image + '"/>' +
+                                    //deleted commentsContainer
+                                    '<div class="panel panel-default">' + //added
+                                    '<div class="panel-heading">Renter Comments</div>'; //added
+                                $.each(item.comments, function(ind, i) {
+                                        html += '<div class="panel-body">' + //added
+                                            '<div class="renterName"><small>' + i.username + '</small></div>' +
+                                            '<div class="renterComment">' + i.comment + '</div>' +
+                                            '<div class="renterStars">';
+
+                                        for (var j = 1; j <= 5; j++) {
+
+                                            if (j <= i.stars) {
+                                                html += '<img src="images/fullStar.png"/>';
+                                            } else {
+                                                html += '<img src="images/emptyStar.png"/>';
+                                            }
+                                        }
+                                        html += '</div>' + //end stars
+                                            '</div>'; //panel body
+                                    }); //each comment
 
                                 html += '</div>' + //panel
                                     '</div>'; //col-md-4
@@ -51,7 +71,7 @@ $(document).ready(function() {
 					// First jQurey Event
 						$("#startRentDate").on("focus", function(){
 						$("#log").append("<br>Search background-color");
-						$(this).css("background-color", "orange");
+						$(this).css("background-color", "blue");
 						})
 
 						.on("blur", function(){
@@ -60,7 +80,7 @@ $(document).ready(function() {
 						});
 
 						$("#submitButton").mouseenter(function(){
-						$(this).text("REHOME!!");
+						$(this).text("ORDER NOW!!");
 						$("#log").append("<br> Button mouseenter");
 							})
 						.on("mouseleave",function(){
