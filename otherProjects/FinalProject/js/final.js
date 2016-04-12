ecbd57c7090d86b6646e24ab82d0b691077596df
ZellisseSDeGuzman <deguzmanszellisse@gmail.com>
2016-04-12 16:59:35 -0400

    test3

--- otherProjects/FinalProject/js/final.js
+++ otherProjects/FinalProject/js/final.js
@@ -31,9 +31,11 @@ $(document).ready(function() {
                         $.each(data, function(index, item) {
                                 html += '<div class="col-xs-12 col-md-4 jsonCat">' +
                                     '<div class="dinoName">' + item.name + '</div>' +
-                                    '<div class="dinoCharacteristics"><small>Characteristics</small>' + item.genre + '</div>' +
+                                    '<div class="dinoCharacteristics"><small>Characteristics </small>' + item.genre + '</div>' +
                                     '<img class="dinoImage" src="' + item.image + '"/>' +
-
+                                    //deleted commentsContainer
+                                    '<div class="panel panel-default">' + //added
+                                    '<div class="panel-heading">Renter Comments</div>'; //added
 
 
                                 html += '</div>' + //panel
